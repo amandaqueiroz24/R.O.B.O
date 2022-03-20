d239e4549f6b7d6464fb9c0169973e2a45c29583
amandaqueiroz24 <amandaqn2016@gmail.com>
2022-03-19 22:09:29 -0300

    Estrutura controller

--- Becomex.Robo.Frontend/Controllers/RoboController.cs
+++ Becomex.Robo.Frontend/Controllers/RoboController.cs
@@ -9,31 +9,96 @@ namespace Becomex.Robo.Frontend.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class WeatherForecastController : ControllerBase
+	public class RoboController : ControllerBase
 	{
-		private static readonly string[] Summaries = new[]
+		
+		[HttpGet("Rest")]
+		public string Rest()
 		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-		};
-
-		private readonly ILogger<WeatherForecastController> _logger;
-
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
-		{
-			_logger = logger;
+			return "O R.O.B.O está em repouso";
 		}
 
-		[HttpGet]
-		public IEnumerable<WeatherForecast> Get()
+		[HttpGet("SlightlyContratedLeft")]
+		public string SlightlyContratedLeft()
 		{
-			var rng = new Random();
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-			{
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = rng.Next(-20, 55),
-				Summary = Summaries[rng.Next(Summaries.Length)]
-			})
-			.ToArray();
+			return "O R.O.B.O está com braço esquerdo levemente contraido";
 		}
+
+
+		[HttpGet("SlightlyContratedRight")]
+		public string SlightlyContratedRight()
+		{
+			return "O R.O.B.O está com braço direito levemente contraido";
+		}
+
+		[HttpGet("ContratedLeft")]
+		public string ContratedLeft()
+		{
+			return "O R.O.B.O está com braço esquerdo levemente contraido";
+		}
+
+		[HttpGet("ContratedRight")]
+		public string ContratedRight()
+		{
+			return "O R.O.B.O está com braço direito levemente contraido";
+		}
+
+		[HttpGet("StronglyContratedLeft")]
+		public string StronglyContratedLeft()
+		{
+			return "O R.O.B.O está com braço esquerdo fortemente contraido";
+		}
+
+		[HttpGet("StronglyContratedRight")]
+		public string StronglyContratedRight()
+		{
+			return "O R.O.B.O está com braço direito fortemente contraido";
+		}
+
+		[HttpGet("RotationToMinus90Left")]
+		public string RotationToMinus90Left()
+		{
+			return "O R.O.B.O está com braço esquerdo com rotação de -90º";
+		}
+
+		[HttpGet("RotationToMinus90Right")]
+		public string RotationToMinus90Right()
+		{
+			return "O R.O.B.O está com braço direito com rotação de -90º";
+		}
+
+		[HttpGet("RotationToMinus45Left")]
+		public string RotationToMinus45Left()
+		{
+			return "O R.O.B.O está com braço esquerdo com rotação de -45º";
+		}
+
+		[HttpGet("RotationTo45Right")]
+		public string RotationTo45Right()
+		{
+			return "O R.O.B.O está com braço direito com rotação de 45º";
+		}
+
+		[HttpGet("RotationTo45Left")]
+		public string RotationTo45Left()
+		{
+			return "O R.O.B.O está com braço esquerdo com rotação de 45º";
+		}
+
+		[HttpGet("RotationTo90Right")]
+		public string RotationTo90Right()
+		{
+			return "O R.O.B.O está com braço direito com rotação de 90º";
+		}
+
+		[HttpGet("RotationTo90Left")]
+		public string RotationTo90Left()
+		{
+			return "O R.O.B.O está com braço esquerdo com rotação de 90º";
+		}
+
+
+
+
 	}
 }
